71c69cea9ab406a233cd008219f030b90c233a10
Haidar Hammoud <haidar.hamoud.17@gmail.com>
2025-02-19 15:23:27 +0200

    Results were printed

--- Homeworks/first-homework.py
+++ Homeworks/first-homework.py
@@ -10,8 +10,17 @@ electricity= (electricity_percentage/100)*salary
 
 total_expense = saving+ rent+ electricity
 remainder= salary- total_expense
-
 rent_year= rent * 12
 electricity_year= electricity*12
-
 salary_power_2= salary **2
+
+print ("The managed finance for ", month, " is as follows:")
+print ("The Total Salary is: ", salary, "$")
+print ("The Saving Amount is: ",saving, "$")
+print ("The Renting Amount is: ", rent, "$")
+print ("The Electricity Bills Amount is: ",electricity, "$")
+print ("Then, the Total Expense will be: ",total_expense, "$")
+print ("The Remainder from your Salary will be: ", remainder, "$")
+print ("The Estiamted Yearly Rent is: ", rent_year,"$")
+print ("The Estimated Yearly Electricity Bills is: ",electricity_year, "$")
+print ("Just for fun!, Your squared salary value is: ", salary_power_2, "$")
