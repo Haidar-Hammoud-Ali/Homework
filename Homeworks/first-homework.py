ef918e946ee830a2280ac9817ee0cb4113cd702f
Haidar Hammoud <haidar.hamoud.17@gmail.com>
2025-02-19 14:38:22 +0200

    expence were calculated

--- Homeworks/first-homework.py
+++ Homeworks/first-homework.py
@@ -1,5 +1,11 @@
 salary= float(input ("Please enter your month salary: "))
 month=  input ("Provide the name of This month: ")
-saving= float(input ("Enter the percentage for saving: "))
-rent=float(input("Enter the percentage for rent: "))
-electricity=float(input("Enter the percentage for electricity: "))
+saving_percentage= float(input ("Enter the percentage for saving: "))
+rent_percentage=float(input("Enter the percentage for rent: "))
+electricity_percentage=float(input("Enter the percentage for electricity: "))
+
+saving=(saving_percentage/100)* salary
+rent =(rent_percentage/100)*salary
+electricity= (electricity_percentage/100)*salary
+
+print(salary, saving, rent, electricity)
